4542f0ac036591045b80ddd126a250591e40d207
Mitch McAffee <themcaffee@gmail.com>
2023-03-15 00:48:20 -0700

    Update template to match new version, README updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # serverless-discord-template
+
+Template for [serverless-discord](https://github.com/themcaffee/serverless-discord)
+
+## Quick start
+
+```
+npm install
+```
+
+Edit `src/index.ts` to start adding your own classes
+
+WIP: Add guide to deploy to lambda
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
-import { ServerlessDiscordCommandChatInput, DiscordInteractionApplicationCommand, DiscordInteractionResponse } from "serverless-discord";
+import { ServerlessDiscordCommandChatInput, DiscordInteractionApplicationCommand, DiscordInteractionResponse, DiscordInteractionResponseTypes } from "serverless-discord";
+import { initLambdaRouter } from "serverless-discord/aws";
 
-class TestCommand extends ServerlessDiscordCommandChatInput {
+// You can get this from the Discord Developer Portal
+const DISCORD_PUBLIC_KEY = process.env?.DISCORD_PUBLIC_KEY || "";
+
+class HelloWorldCommand extends ServerlessDiscordCommandChatInput {
     constructor() {
         super({
             name: "test",
             options: [],
         });
     }
-    handleInteraction(interaction: DiscordInteractionApplicationCommand): DiscordInteractionResponse {
+    async handleInteraction(interaction: DiscordInteractionApplicationCommand): Promise<DiscordInteractionResponse> {
         return {
-            type: 1,
+            type: DiscordInteractionResponseTypes.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
-                tts: false,
-                content: "test",
-                embeds: [],
-                allowed_mentions: {
-                    parse: [],
-                    roles: [],
-                    users: [],
-                    replied_user: false,
-                },
-                components: [],
+                content: "Hello World!",
             },
         }
     }
 }
+
+const router = initLambdaRouter({ commands: [new HelloWorldCommand()], applicationPublicKey: DISCORD_PUBLIC_KEY });
