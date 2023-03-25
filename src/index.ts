75f261c0aa60030cefc5ed182629d212fd8e7d3f
Mitch McAffee <themcaffee@gmail.com>
2023-03-25 01:29:27 -0700

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import { initLambdaRouter } from "serverless-discord/lambda/router";
 import { HelloWorldCommand } from "./commands";
 import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 
-// Get DISCORD_PUBLIC_KEY from Parameter Store secure string
+// Initialize the AWS SDK SSM client
 const client = new SSMClient({ region: process.env.AWS_REGION });
 
 export const lambdaHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  // Get DISCORD_PUBLIC_KEY from Parameter Store secure string
   const res = await client.send(new GetParameterCommand({ Name: process.env.DISCORD_PUBLIC_KEY || "", WithDecryption: true }));
   const applicationPublicKey = res.Parameter?.Value || "";
-
-  console.log(`public key length: ${applicationPublicKey.length}`);
 
   // Initialize the router with the command and the public key of your application.
   const router = initLambdaRouter({ 
